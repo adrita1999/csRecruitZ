af2edd2f405e2bfbe178b91f801e85ad88474a92
Dristi123 <64282800+Dristi123@users.noreply.github.com>
2022-07-07 02:14:50 +0600

    some_basic_js_files_added

--- csRecruitz_codebase/react_frontend/src/index.js
+++ csRecruitz_codebase/react_frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Router,BrowserRouter} from "react-router-dom";
+
+function Router(){
+    return
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ csRecruitz_codebase/react_frontend/src/my_components/Compo.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+function Compo(props) {
+    return (
+        <div>
+            <h2>my component {props.name}</h2>
+            <button>Click Me</button>
+        </div>
+    )
+}
+export default Compo;
