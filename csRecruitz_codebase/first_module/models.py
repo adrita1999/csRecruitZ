f5f43782c94ffaaa97cb5aa7387ae8a0435c9f58
adrita1999 <72884537+adrita1999@users.noreply.github.com>
2022-07-14 22:19:03 +0600

    database updated

--- csRecruitz_codebase/first_module/models.py
+++ csRecruitz_codebase/first_module/models.py
@@ -44,5 +44,38 @@ class Jobseeker(User):
     pref_sal = models.IntegerField(unique=True)
     resume = models.FileField(upload_to='resumes', null=True)
 
+class NewJobpost(models.Model):
+    jobpost_id = models.IntegerField(primary_key=True)
+    employer_id = models.ForeignKey(Employer,  on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    post_date = models.DateField()
+    deadline_date = models.DateField()
+    salary = models.IntegerField()
+    required_experience = models.IntegerField()
+    vacancies = models.IntegerField()
+    job_context = models.TextField()
+    job_nature = models.TextField()
+    job_responsibilities = models.TextField()
+    edu_requirement = models.TextField()
+    additional_requirements = models.TextField()
+    application_process = models.TextField()
 
 
+class Skill(models.Model):
+    skill_id = models.IntegerField(primary_key=True)
+    skill_name = models.CharField(max_length=100)
+    gap_between_consecutive_attempts = models.IntegerField()
+
+
+class JobSeekerSkill(models.Model):
+    jobseeker_skill_id = models.IntegerField(primary_key=True)
+    user_id = models.ForeignKey(User,  on_delete=models.CASCADE)
+    skill_id = models.ForeignKey(Skill,  on_delete=models.CASCADE)
+    isOpenToWork = models.BooleanField(default=False)
+
+
+class JobSkill(models.Model):
+    job_skill_id = models.IntegerField(primary_key=True)
+    jobpost_id = models.ForeignKey(Jobpost,  on_delete=models.CASCADE)
+    skill_id = models.ForeignKey(Skill,  on_delete=models.CASCADE)
