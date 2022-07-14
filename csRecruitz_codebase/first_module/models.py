154fbbd2d590e2b240638a39389cdfdd040ffb9f
adrita1999 <72884537+adrita1999@users.noreply.github.com>
2022-07-14 08:14:06 +0600

    database table created and data inserted

--- csRecruitz_codebase/first_module/models.py
+++ csRecruitz_codebase/first_module/models.py
@@ -4,3 +4,45 @@ from django.db import models
 class Jobpost(models.Model):
     title=models.CharField(max_length=200)
 
+
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=40)
+    email = models.EmailField(unique=True)
+    # address
+    thana = models.CharField(max_length=30, blank=True, null=True)
+    district = models.CharField(max_length=30)
+    division = models.CharField(max_length=30)
+
+
+class UserContact(models.Model):
+    user_contact_id = models.IntegerField(primary_key=True)
+    user_id = models.IntegerField()
+    contact_no = models.CharField(max_length=15)
+
+    class Meta:
+        unique_together = (("user_id", "contact_no"),)
+
+
+class Employer(User):
+    org_type = models.CharField(max_length=20)
+    establishment_year = models.IntegerField()
+
+
+class Jobseeker(User):
+    father_name = models.CharField(max_length=40)
+    mother_name = models.CharField(max_length=40)
+    self_desc = models.CharField(max_length=200)
+    propic = models.ImageField(upload_to='images', null=True)
+    date_of_birth = models.DateField()
+    nationality = models.CharField(max_length=20)
+    field = models.CharField(max_length=20)
+    nid_number = models.IntegerField(unique=True)
+    pref_job_ntr = models.CharField(max_length=20)
+    pref_org_type = models.CharField(max_length=20)
+    pref_sal = models.IntegerField(unique=True)
+    resume = models.FileField(upload_to='resumes', null=True)
+
+
+
--- csRecruitz_codebase/first_module/views.py
+++ csRecruitz_codebase/first_module/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render,HttpResponse
-from .models import Jobpost
+from .models import *
 from .serializers import postSerializer
 from  rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import viewsets
 
+user = Jobseeker(user_id=1, name="Adrita Hossain Nakshi", email="adrita_99@yahoo.com", password="1234", thana="Boalia", district="Rajshahi", division="Rajshahi", father_name="Dr. Md. Elias Hossain", mother_name="Dr. Zennat Ferdousi", date_of_birth="1999-02-06", self_desc="I am a CS under-graduate.", nationality="Bangladeshi", nid_number="12345678", field="Teaching", pref_sal="50000", pref_job_ntr="Full-time", pref_org_type="Government", propic="propics_input/nakshi.jpeg", resume="resumes_input/nakshi.docx")
+user.save()
+emp = Employer(user_id=2, name="Optimizely", email="optimizely@gmail.com", password="1234", district="Dhaka", division="Dhaka", org_type="Non-government", establishment_year="2005")
+emp.save()
+emp = Employer(user_id=3, name="Kona SL", email="kona@yahoo.com", password="1234", district="Dhaka", division="Dhaka", org_type="Non-government", establishment_year="2001")
+emp.save()
+emp = Employer(user_id=4, name="Data Edge Ltd", email="dataedge@gmail.com", password="1234", district="Dhaka", division="Dhaka", org_type="Non-government", establishment_year="1996")
+emp.save()
+emp = Employer(user_id=5, name="Samsung", email="samsung@gmail.com", password="1234", district="Dhaka", division="Dhaka", org_type="Non-government", establishment_year="1981")
+emp.save()
+emp = Employer(user_id=6, name="Intelligent Machines Limited", email="iml@gmail.com", password="1234", district="Dhaka", division="Dhaka", org_type="Non-government", establishment_year="2015")
+emp.save()
+emp = Employer(user_id=7, name="BEPRC", email="beprc@gmail.com", password="1234", district="Dhaka", division="Dhaka", org_type="Government", establishment_year="1998")
+emp.save()
+emp = Employer(user_id=8, name="Bangladesh Airforce", email="airbd@gmail.com", password="1234", district="Dhaka", division="Dhaka", org_type="Government", establishment_year="1975")
+emp.save()
+contact = UserContact(user_contact_id=1, user_id=1, contact_no="01878046439")
+contact.save()
+contact = UserContact(user_contact_id=2, user_id=1, contact_no="01718464397")
+contact.save()
 
 class postViewsets(viewsets.ModelViewSet):
     # def list(self,request):
