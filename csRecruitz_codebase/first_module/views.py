c679daf73543bec8b23499fda03cacde6d722d73
Dristi123 <64282800+Dristi123@users.noreply.github.com>
2022-08-14 01:14:00 +0600

    question_added

--- csRecruitz_codebase/first_module/views.py
+++ csRecruitz_codebase/first_module/views.py
@@ -1773,8 +1773,8 @@ question9 = Question(question_id=9, skill_id=skill1, question_text=" Which of th
                      answer="3",
                      time_limit="2:00")
 question9.save()
-question10 = Question(question_id=10, skill_id=skill1, question_text=" What happens if the following C++ statement is compiled and executed?"
-                                                                     "int *ptr = NULL;"
+question10 = Question(question_id=10, skill_id=skill1, question_text=" What happens if the following C++ statement is compiled and executed?\n"
+                                                                     "int *ptr = NULL;\n"
                                                                      "delete ptr;",
                      optionA="The program is not semantically correct",
                      optionB="The program is compiled and executed successfully",
@@ -1784,6 +1784,53 @@ question10 = Question(question_id=10, skill_id=skill1, question_text=" What happ
                      answer="2",
                      time_limit="2:00")
 question10.save()
+question11 = Question(question_id=11, skill_id=skill1, question_text="What is the difference between delete and delete[] in C++?",
+                     optionA="delete is syntactically correct but delete[] is wrong and hence will give an error if used in any case",
+                     optionB="delete is used to delete normal objects whereas delete[] is used to pointer objects",
+                     optionC="delete is a keyword whereas delete[] is an identifier",
+                     optionD="delete is used to delete single object whereas delete[] is used to multiple(array/pointer of) objects",
+                     mark=1,
+                     answer="4",
+                     time_limit="1:30")
+question11.save()
+question12 = Question(question_id=12, skill_id=skill1, question_text=" What happens if the following program is executed in C and C++?\n #include <stdio.h> \n int main(void){\nint new = 5;\nprintf(\"%d\", new); }",
+                     optionA="Error in C and successful execution in C++",
+                     optionB="Error in both C and C++",
+                     optionC="Error in C++ and successful execution in C",
+                     optionD="A successful run in both C and C++",
+                     mark=2,
+                     answer="3",
+                     time_limit="2:00")
+question12.save()
+question13 = Question(question_id=13, skill_id=skill1, question_text="Which of the following type is provided by C++ but not C?",
+                     optionA="double",
+                     optionB="float",
+                     optionC="int",
+                     optionD="bool",
+                     mark=1,
+                     answer="4",
+                     time_limit="1:30")
+question13.save()
+question14 = Question(question_id=14, skill_id=skill1, question_text="By default, all the files in C++ are opened in _________ mode.",
+                     optionA="Binary",
+                     optionB="VTC",
+                     optionC="Text",
+                     optionD="ISCII",
+                     mark=1,
+                     answer="3",
+                     time_limit="1:30")
+question14.save()
+question15 = Question(question_id=15, skill_id=skill1, question_text="What is the size of wchar_t in C++?",
+                     optionA="Based on the number of bits in the system",
+                     optionB="2 or 4",
+                     optionC="4",
+                     optionD="2",
+                     mark=2,
+                     answer="1",
+                     time_limit="2:00")
+question15.save()
+
+
 
 job_exp1 = JobExperience(jobexperience_id=1, experience_name="Lecturer", organization_name="UIU", from_year="2017",
                          to_year="2018", user_id=user1)
